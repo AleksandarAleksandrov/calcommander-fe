22578a43c96a6471a6b7fb63c6906324f6110acc
AleksandarAleksandrov <amaleksandrov94@gmail.com>
2025-05-21 12:24:24 +0300

    add redux store

---
+++ src/components/Counter.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useAppSelector, useAppDispatch } from '../store/hooks';
+import { increment, decrement, incrementByAmount, selectCount } from '../store/slices/counterSlice';
+
+export function Counter() {
+  const count = useAppSelector(selectCount);
+  const dispatch = useAppDispatch();
+  const [incrementAmount, setIncrementAmount] = React.useState('2');
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+      </div>
+      <div>
+        <input
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={e => setIncrementAmount(e.target.value)}
+        />
+        <button
+          onClick={() => dispatch(incrementByAmount(Number(incrementAmount) || 0))}
+        >
+          Add Amount
+        </button>
+      </div>
+    </div>
+  );
+} 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Counter } from '../components/Counter';
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to CalCommander</p>
+      <div>
+        <h2>Redux Counter Example</h2>
+        <Counter />
+      </div>
     </div>
   );
 };
