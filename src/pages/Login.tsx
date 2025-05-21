3554db7b3b3ce77777bbdc4ccbade253af84ca2b
AleksandarAleksandrov <amaleksandrov94@gmail.com>
2025-05-21 15:39:17 +0300

    calling the action

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { AnyAction } from 'redux';
+import { ThunkDispatch } from '@reduxjs/toolkit';
+import { loginAction, LoginPayload } from '../store/user/userActions';
 
-class Login extends Component {
+interface LoginState {
+  email: string;
+  password: string;
+}
 
-  
+interface LoginDispatchProps {
+  login: (credentials: LoginPayload) => void;
+}
+
+type Props = LoginDispatchProps;
+
+class Login extends Component<Props, LoginState> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    this.setState({
+      ...this.state,
+      [name]: value
+    } as Pick<LoginState, keyof LoginState>);
+  }
+
+  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const credentials: LoginPayload = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    
+    // Dispatch the login action with the form data
+    this.props.login(credentials);
+  }
 
   render() {
     return (
       <div className="login-container">
         <h1>Login</h1>
-        <form className="login-form">
+        <form className="login-form" onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <input
               type="input"
               id="email"
+              name="email"
               placeholder="Enter your email"
               className="form-control"
+              value={this.state.email}
+              onChange={this.handleChange}
             />
           </div>
           <div className="form-group">
@@ -23,8 +65,11 @@ class Login extends Component {
             <input
               type="password"
               id="password"
+              name="password"
               placeholder="Enter your password"
               className="form-control"
+              value={this.state.password}
+              onChange={this.handleChange}
             />
           </div>
           <button type="submit" className="login-button">
@@ -36,4 +81,12 @@ class Login extends Component {
   }
 }
 
-export default Login; 
+const mapDispatchToProps = (dispatch: ThunkDispatch<any, unknown, AnyAction>): LoginDispatchProps => {
+  return {
+    login: (credentials: LoginPayload) => {
+      dispatch(loginAction(credentials as any));
+    }
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Login); 
--- src/store/user/userActions.ts
+++ src/store/user/userActions.ts
@@ -5,4 +5,9 @@ export const LOGIN_REQUEST = `[API]${LOGIN}_REQUEST`;
 export const LOGIN_SUCCESS = `[API]${LOGIN}_SUCCESS`;
 export const LOGIN_FAILURE = `[API]${LOGIN}_FAILURE`;
 
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
+
 export const loginAction = createApiThunk(LOGIN, '/login', 'POST');
