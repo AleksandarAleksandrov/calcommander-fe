bd4fba6b771f8ce27511622b713124500c8637a5
AleksandarAleksandrov <amaleksandrov94@gmail.com>
2025-05-27 22:32:39 +0300

    more config

--- src/signin/SignInPage.tsx
+++ src/signin/SignInPage.tsx
@@ -1,19 +1,32 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 import { SignInForm } from './SignInForm';
+import womanPointingCalendar from '@/assets/images/woman_pointing_at_calendar.png';
 
 export default function SignInPage() {
 
   return (
     <Box
       minH="100vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
+      display="grid"
+      gridTemplateColumns="1fr 1fr 1fr"
       bg="gray.50"
       p={4}
     >
-      <SignInForm
-      />
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Image src={womanPointingCalendar} alt="logo"/>
+      </Box>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <SignInForm/>
+      </Box>
+      <Box />
     </Box>
   );
 }; 
