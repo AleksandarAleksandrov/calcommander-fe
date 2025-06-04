34bcc24869149332536aafdc78b5422109d13069
AleksandarAleksandrov <amaleksandrov94@gmail.com>
2025-06-04 11:37:36 +0100

    add user/me logic

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,14 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createApiThunk } from "./thunkApiFactory";
+
+export const USER_ME = 'USER_ME';
+export const USER_ME_REQUEST = `${USER_ME}_REQUEST`;
+export const USER_ME_SUCCESS = `${USER_ME}_SUCCESS`;
+export const USER_ME_FAILURE = `${USER_ME}_FAILURE`;
 
 const userSlice = createSlice({
     name: '@@user',
     initialState: {
         user: null,
+        hasCompletedOnboarding: false,
         isLoading: false,
     },
     reducers: {
 
+    },
+    extraReducers: (builder) => {
+        builder.addCase(USER_ME_REQUEST, (state) => {
+            state.isLoading = true;
+        });
+        builder.addCase(USER_ME_SUCCESS, (state, action: any) => {
+            state.user = action.payload.data;
+            state.isLoading = false;
+        });
+        builder.addCase(USER_ME_FAILURE, (state) => {
+            state.isLoading = false;
+        });
     }
 });
+
+export const getUserMe = createApiThunk(USER_ME, '/users/me');
 
 export default userSlice.reducer;
