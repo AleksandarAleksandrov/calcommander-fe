f069f940ced0238615c193c39160be7165ff55b4
AleksandarAleksandrov <amaleksandrov94@gmail.com>
2025-06-04 15:45:32 +0100

    actions

--- src/store/onboardingSlice.ts
+++ src/store/onboardingSlice.ts
@@ -1,4 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createApiThunk } from "./thunkApiFactory";
+
+const GET_ONBOARDING_DATA = 'GET_ONBOARDING_DATA';
+const GET_ONBOARDING_DATA_REQUEST = `${GET_ONBOARDING_DATA}_REQUEST`;
+const GET_ONBOARDING_DATA_SUCCESS = `${GET_ONBOARDING_DATA}_SUCCESS`;
+const GET_ONBOARDING_DATA_FAILURE = `${GET_ONBOARDING_DATA}_FAILURE`;
 
 export enum OnboardingStep {
     USER_INFO = 'USER_INFO',
@@ -11,14 +17,49 @@ const onboardingSlice = createSlice({
     name: 'onboarding',
     initialState: {
         step: OnboardingStep.USER_INFO,
+        isLoading: true,
+        calendars: [],
+        availability: [],
+        userData: {
+            slug: null,
+            name: null,
+            timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+        }
     },
     reducers: {
         setStep: (state, action) => {
             state.step = action.payload;
         },
+        setUserData: (state, action) => {
+            state.userData = action.payload;
+        },
+        setCalendars: (state, action) => {
+            state.calendars = action.payload;
+        },
+        setAvailability: (state, action) => {
+            state.availability = action.payload;
+        }
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(GET_ONBOARDING_DATA_REQUEST, (state, action) => {
+                state.isLoading = true;
+            })
+            .addCase(GET_ONBOARDING_DATA_SUCCESS, (state, action: any) => {
+                state.isLoading = false;
+                state.calendars = action.payload.data.calendars;
+                state.availability = action.payload.data.availability;
+                state.userData = action.payload.data.userData;
+            })
+            .addCase(GET_ONBOARDING_DATA_FAILURE, (state, action) => {
+                state.isLoading = false;
+            })
+
+    }
 });
 
-export const { setStep } = onboardingSlice.actions;
+export const { setStep, setUserData, setCalendars, setAvailability } = onboardingSlice.actions;
+
+export const getOnboardingData = createApiThunk(GET_ONBOARDING_DATA, '/onboarding');
 
 export default onboardingSlice.reducer;
